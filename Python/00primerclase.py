153b7dce14abb82615a792caf57448325d21b519
keviin00 <90279937+keviin00@users.noreply.github.com>
2023-06-01 00:05:33 -0300

    Add files via upload

---
+++ Python/00primerclase.py
@@ -0,0 +1,13 @@
+'''print("SHACO, KHAZIX, TALON, RENGAR, NOCTURNE, KAYN") # asi se comenta en python
+"""
+comentario de varias lineas 
+"""
+print(19)#int
+print(1.5)#floar'''
+
+my_name = "Kevin"
+my_nick_name = "xSHAx"
+my_elo= "Platinum"
+my_main="Shaco"
+
+print('Me llamo', my_name, 'mi nombre de invocador es', my_nick_name )

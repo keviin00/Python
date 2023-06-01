0906082e7d114b922b2aa3eb7277397cd8d59cf2
keviin00 <90279937+keviin00@users.noreply.github.com>
2023-06-01 15:06:04 -0300

    link del video
    https://www.youtube.com/watch?v=Kp4Mvapo5kc&t=3665s

---
+++ Python/01-Variables.py
@@ -0,0 +1,15 @@
+
+digimon_name = "Patamon"
+digimon_attribute = "Luz"
+digimon_emblem = "Esperanza"
+digimon_buddy = "Takeru Takaishi"
+digimon_exp_evolve = 512
+
+x = input("Ingresa el digimon que quiere consultar es ")
+
+if x == digimon_name:
+    x = digimon_name
+print("El nombre del ingresado digimon es",x, "\nSu atributo es",digimon_attribute,
+       "\nSu emblema es",digimon_emblem,"\nEl niño elegido que lo acompaña es",digimon_buddy,
+       "\nLa experiencia necesaria para DigiEvolucionar es",digimon_exp_evolve)
+
